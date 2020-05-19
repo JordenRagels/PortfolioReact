388f504f99c0da0de58c64bdffae8f5d47862f17
Jorden Ragels <jordenragels3@gmail.com>
2020-05-19 00:11:06 -0600

    side drawer logo

--- balancever/src/components/SideDrawer/SideDrawer.js
+++ balancever/src/components/SideDrawer/SideDrawer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './SideDrawer.css'
+import SideLogo from '../../images/full-logo.png'
 
 const sideDrawer = props => (
     <nav className='side-drawer'>
+    <div className='sideDrawerLogo'>
+            <a href='/'>
+            <img src= { SideLogo } alt='logo'></img>
+            </a>
+            </div>
         <ul>
             <li>
                 <a href='/'>Structure Your Day</a>
